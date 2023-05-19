79b60d252177667aacddf6c19b94332afdd3373f
EruliaF Gunwardana <nisal.nmg@gmail.com>
2023-05-19 19:32:39 +0530

    added postman doc

--- frontend-client/src/components/modules/manage-device/manage-device.routes.js
+++ frontend-client/src/components/modules/manage-device/manage-device.routes.js
@@ -1,14 +1,9 @@
 import GatewayPage from './pages/GatewayPage';
-import DevicePage from './pages/DevicePage';
 
 const manageDeviceRoutes = [
         {
             path: '/',
             element: <GatewayPage />
-        },
-        {
-            path: '/device',
-            element: <DevicePage />
         }
 ];
 
